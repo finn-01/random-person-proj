f0f1b5a382b49b5fb17be9fa2938ad53de08b7d9
finn-01 <nguyensam576@gmail.com>
2022-01-21 23:30:08 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,73 @@ const App = () => {
 		getPerson();
 	}, []);
 
-	return <div>App</div>;
+	const handleValue = (e) => {
+		if (e.target.classList.contains("icon")) {
+			const newValue = e.target.dataset.label;
+			setTitle(newValue);
+			setValue(person[newValue]);
+		}
+	};
+
+	return (
+		<main>
+			<div className="block bcg-black"></div>
+			<div className="block">
+				<div className="container">
+					<img
+						src={(person && person.image) || imgDefaul}
+						alt="img"
+						className="user-img"
+					/>
+					<p className="user-title">My {title} is</p>
+					<p className="user-value">{value}</p>
+					<div className="values-list">
+						<button
+							className="icon"
+							data-label="name"
+							onMouseOver={handleValue}
+						>
+							<FaUser />
+						</button>
+						<button
+							className="icon"
+							data-label="email"
+							onMouseOver={handleValue}
+						>
+							<FaEnvelopeOpen />
+						</button>
+						<button className="icon" data-label="age" onMouseOver={handleValue}>
+							<FaCalendarTimes />
+						</button>
+						<button
+							className="icon"
+							data-label="street"
+							onMouseOver={handleValue}
+						>
+							<FaMap />
+						</button>
+						<button
+							className="icon"
+							data-label="phone"
+							onMouseOver={handleValue}
+						>
+							<FaPhone />
+						</button>
+						<button
+							className="icon"
+							data-label="password"
+							onMouseOver={handleValue}
+						>
+							<FaLock />
+						</button>
+					</div>
+					<button className="btn" type="button" onClick={getPerson}>
+						{loading ? "loading..." : "random user"}
+					</button>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default App;
