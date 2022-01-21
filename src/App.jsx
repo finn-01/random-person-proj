ab98aa8ca3adfdca7dfcecfe1a7fea63e534af07
finn-01 <nguyensam576@gmail.com>
2022-01-21 23:10:34 +0700

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
 	FaEnvelopeOpen,
 	FaUser,
@@ -8,7 +8,57 @@ import {
 	FaLock,
 } from "react-icons/fa";
 
+const url = "https://randomuser.me/api/";
+const imgDefaul = "https://randomuser.me/api/portraits/men/75.jpg";
+
 const App = () => {
+	const [loading, setLoading] = useState(false);
+	const [person, setPerson] = useState(null);
+	const [value, setValue] = useState("random person");
+	const [title, setTitle] = useState("name");
+
+	const getPerson = async () => {
+		setLoading(true);
+
+		const response = await fetch(url);
+		const data = await response.json();
+
+		//console.log(data);
+
+		const person = data.results[0];
+		//console.log(person);
+
+		const { phone, email } = person;
+		const { large: image } = person.picture;
+		const { password } = person.login;
+		const { first, last } = person.name;
+		const {
+			dob: { age },
+		} = person;
+		const {
+			street: { number, name },
+		} = person.location;
+
+		const newPerson = {
+			image,
+			phone,
+			email,
+			password,
+			age,
+			street: `${number}${name}`,
+			name: `${first}${last}`,
+		};
+
+		setPerson(newPerson);
+		setLoading(false);
+		setTitle("name");
+		setValue(newPerson.name);
+	};
+
+	useEffect(() => {
+		getPerson();
+	}, []);
+
 	return <div>App</div>;
 };
 
